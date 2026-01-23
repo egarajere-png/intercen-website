abd13237f384444f76d10fe23e216a9bd79de914
Dennis Wanjiku <131625108+mwangiiii@users.noreply.github.com>
2026-01-23 11:35:47 +0300

    fix: corrected the adjustment of the /profile-section route

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import { FeaturedBooks } from '@/components/home/FeaturedBooks';
 import { CategorySection } from '@/components/home/CategorySection';
 import { BestsellerSection } from '@/components/home/BestsellerSection';
 import { PromoBanner } from '@/components/home/PromoBanner';
-import ProfileSetup from './ProfileSetup';
+
 
 const Index = () => {
   return (
@@ -14,7 +14,7 @@ const Index = () => {
       <CategorySection />
       <PromoBanner />
       <BestsellerSection />
-      <Route path="/profile-setup" element={<ProfileSetup />} />
+      
     </Layout>
   );
 };
